caeca51cad9d2da3361a923b020e30fca63bf6fb
Andrew McBurney <andrewrobertmcburney@gmail.com>
2017-06-02 18:17:34 -0700

    Modified main GraphQL query.

--- github_graphql.rb
+++ github_graphql.rb
@@ -6,6 +6,7 @@ require 'graphql/client'
 require 'graphql/client/http'
 require 'open-uri'
 
+# Configure the HTTPAdapter
 HTTPAdapter = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
   def headers(_context)
     { 'Authorization' => "Bearer #{ENV['GITHUB_TOKEN']}" }
@@ -22,9 +23,27 @@ locations = %w[san-francisco san-jose berkley oakland san-bruno san-mateo]
 location_string = locations.map { |language| "location:#{language}" }.join(' ')
 language_string = languages.map { |language| "language:#{language}" }.join(' ')
 
+query_variables = {
+  query_string: language_string + ' ' + location_string,
+  user_limit: 10
+}
+
+UserCountQuery = Client.parse <<-'GRAPHQL'
+  query($query_string: String!) {
+    search(query: $query_string, type: USER, first: 1) {
+      userCount
+    }
+  }
+GRAPHQL
+
+# Execute the query and parse the result
+total_users = Client.query(UserCountQuery, variables: query_variables)
+                    .data.search.user_count
+puts total_users
+
 UserQuery = Client.parse <<-'GRAPHQL'
   query($query_string: String!) {
-    search(query: $query_string, type: USER, first: 10) {
+    search(query: $query_string, type: USER, first: 20) {
       userCount
       edges {
         cursor
@@ -44,16 +63,23 @@ UserQuery = Client.parse <<-'GRAPHQL'
             organizations(first: 10) {
               nodes {
                 name
-                teamsUrl
               }
             }
-            repositories(first: 100, orderBy: {field: PUSHED_AT, direction: DESC}) {
-              nodes {
-                name
-                url
-                languages(first: 5, orderBy: {field: SIZE, direction:DESC}) {
-                  nodes {
-                    name
+            repositories(first: 100, orderBy:
+                          { field: PUSHED_AT, direction: DESC }) {
+              totalCount
+              edges {
+                cursor
+                node {
+                  name
+                  url
+                  isFork
+                  isMirror
+                  languages(first: 5, orderBy:
+                             { field: SIZE, direction: DESC }) {
+                    nodes {
+                      name
+                    }
                   }
                 }
               }
@@ -64,11 +90,6 @@ UserQuery = Client.parse <<-'GRAPHQL'
     }
   }
 GRAPHQL
-
-query_variables = {
-  query_string: language_string + ' ' + location_string,
-  user_limit: 10
-}
 
 # Execute the query and parse the result
 result = Client.query(UserQuery, variables: query_variables)
@@ -87,11 +108,11 @@ a = result.data.search.edges.map do |edge|
     follower_count: user.followers.total_count,
     is_hireable: user.is_hireable,
     organizations: user.organizations.nodes.map(&:name),
-    repositories: user.repositories.nodes.map do |repo|
+    repositories: user.repositories.edges.map do |repo|
       {
-        name: repo.name,
-        url: repo.url,
-        languages: repo.languages.nodes.map(&:name)
+        name: repo.node.name,
+        url: repo.node.url,
+        languages: repo.node.languages.nodes.map(&:name)
       }
     end
   }
